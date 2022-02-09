c352df4e2c20d7a6b222297552151703a3af7c7a
Nikhilsai10 <99253063+Nikhilsai10@users.noreply.github.com>
2022-02-09 20:15:05 +0530

    Assignment-01. Even or odd

---
+++ biggestof2.py
@@ -0,0 +1,8 @@
+Num01= int(input("Enter a number:"))
+Num02= int(input("Enter a number:"))
+if Num01>Num02:
+   print("Number-01 =",Num01," is Big")
+elif Num01<Num02:
+   print("Number-02 =",Num02," is Big")
+else:
+    print("Number-01,Number-02,",Num01,Num02,"are equal")
---
+++ eood.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("The entered number {0} is Even".format(num))
+else:
+   print("The entered number {0} is Odd".format(num))
